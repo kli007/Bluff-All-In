a9c00aa1f42b755572a400bb120d0e4a1863fb0e
kli007 <158181462+kli007@users.noreply.github.com>
2026-08-20 18:27:19 -0400

    refine control
    added attack 1 and refined all other movement code

--- player.gd
+++ player.gd
@@ -1,41 +1,33 @@
 extends CharacterBody2D
 
-
 @export var speed : float = 200.0
 @export var jump_velocity : float = -350.0
-var direction#: Vector2 = Vector2.ZERO
-var was_in_air : bool = false
-var animation_locked : bool = false
+
 @onready var animation = $AnimationPlayer
+var attack = false
+var direction
 
 func _physics_process(delta: float) -> void:
-	direction = 0
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		was_in_air = true
-	else:
-		if was_in_air == true:
-			print(1)
-		was_in_air = false
-
-	if Input.is_action_pressed("Move_Left"):
-		direction = -1
-		$Sprite2D.flip_h = true
-	if Input.is_action_pressed("Move_Right"):
-		direction = 1
-		$Sprite2D.flip_h = false
 		
+	direction = Input.get_axis("Move_Left", "Move_Right")
 	if Input.is_action_just_pressed("Move_Jump") and is_on_floor():
-		jump()
+		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("Attack") and is_on_floor():
+		attack = true
+		animation.play("Attack_1")
 		
 	if direction and animation.name != 'Jump':
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
-	animation_control()
-	move_and_slide()
+	if not attack:
+		move_and_slide()
+		animation_control()
+		flip_sprite()
 	
 func animation_control():
 	if velocity.y < 0:
@@ -47,16 +39,12 @@ func animation_control():
 	else:
 		animation.play('Idle')
 	
-func jump():
-	velocity.y = jump_velocity
-	animation.play("Jump")
-	animation_locked = true
-
-func land():
-	animation.play("Fall")
-	animation_locked = true
-	
+func flip_sprite():
+	if direction < 0:
+		$Sprite2D.flip_h = true
+	if direction > 0:
+		$Sprite2D.flip_h = false
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "Jump":
-		animation_locked = false
+	if anim_name == "Attack_1":
+		attack = false
