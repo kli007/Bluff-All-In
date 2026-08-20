cb9bb5fc33db094cf17993cad61c10c58774cc6b
kli007 <158181462+kli007@users.noreply.github.com>
2026-08-20 15:46:39 -0400

    created basic character
    create testmap, character, left right, jump

---
+++ player.gd
@@ -0,0 +1,62 @@
+extends CharacterBody2D
+
+
+@export var speed : float = 200.0
+@export var jump_velocity : float = -350.0
+var direction#: Vector2 = Vector2.ZERO
+var was_in_air : bool = false
+var animation_locked : bool = false
+@onready var animation = $AnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	direction = 0
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		was_in_air = true
+	else:
+		if was_in_air == true:
+			print(1)
+		was_in_air = false
+
+	if Input.is_action_pressed("Move_Left"):
+		direction = -1
+		$Sprite2D.flip_h = true
+	if Input.is_action_pressed("Move_Right"):
+		direction = 1
+		$Sprite2D.flip_h = false
+		
+	if Input.is_action_just_pressed("Move_Jump") and is_on_floor():
+		jump()
+		
+	if direction and animation.name != 'Jump':
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	animation_control()
+	move_and_slide()
+	
+func animation_control():
+	if velocity.y < 0:
+		animation.play('Jump')
+	elif velocity.y > 0:
+		animation.play('Fall')
+	elif velocity.x != 0:
+		animation.play('Move')
+	else:
+		animation.play('Idle')
+	
+func jump():
+	velocity.y = jump_velocity
+	animation.play("Jump")
+	animation_locked = true
+
+func land():
+	animation.play("Fall")
+	animation_locked = true
+	
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Jump":
+		animation_locked = false
