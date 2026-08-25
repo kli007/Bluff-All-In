3d807a224d2c7fa3def98dc779c982c214e0bff8
kli007 <158181462+kli007@users.noreply.github.com>
2026-08-25 12:14:19 -0400

    simplified variable naming
    created a function to define the node dicts, runs when main is ready so all nodes would be loaded.
    switch setpendcards and setplayedcards buttons

--- player.gd
+++ player.gd
@@ -26,11 +26,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Move_Jump") and is_on_floor():
 		velocity.y = jump_velocity
 	if Input.is_action_just_pressed("Attack") and is_on_floor():
-		setPendCard()
+		setPlayedCard()
 		attack = true
 		animNode.play("Attack_1")
 	if Input.is_action_just_pressed("Trick") and is_on_floor():
-		setPlayedCard()
+		setPendCard()
 		
 	if direction and animNode.name != 'Jump':
 		velocity.x = direction * speed
@@ -59,23 +59,23 @@ func flip_sprite() -> void:
 		$Sprite2D.flip_h = false
 		
 func setPendCard() -> void:
-	var pendNodes: Dictionary = pendCards.nodeDict
 	while CardData.checkSpace(pendDict):
 		CardData.setCards(cardNode.moveCard(), 
 		pendDict, pendNodes)
 		
 func setPlayedCard() -> void:
-	var pendNodes: Dictionary = pendCards.nodeDict
-	var playedNodes: Dictionary = playedCards.nodeDict
 	if CardData.checkSpace(playedDict) and pendDict['Card1'] != null:
 		CardData.setCards(cardNode.playCard(pendDict, pendNodes), playedDict, playedNodes)
 		moveUpPendCards()
 
 func moveUpPendCards() -> void:
-	var pendNodes: Dictionary = pendCards.nodeDict
 	CardData.moveUpCards(pendDict, pendNodes)
 	setPendCard()
-		
+
+
+func defineNodeVariables():
+	pendNodes = pendCards.nodeDict
+	playedNodes = playedCards.nodeDict
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "Attack_1":
